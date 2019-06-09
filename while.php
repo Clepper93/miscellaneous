1a4332c6d5aeee214fc93c5a224dfcb1dbe3ad54
Clepper Design <33584689+clepperdesign@users.noreply.github.com>
2019-06-09 08:31:53 -0700

    it can't be said that this isn't php code

---
+++ while.php
@@ -0,0 +1,19 @@
+<!DOCTPYE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>PHP Test</title>
+</head>
+<body>
+<h2>Fun with PHP</h2>
+<?php 
+$counter = 0;
+while($counter<=10){
+    if ($counter % 2 == 0){
+        echo "$counter is even" . "<br>";
+    } else{
+        echo "$counter is odd"."<br>";
+    }
+$counter++;
+}
+?>
